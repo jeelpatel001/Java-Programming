d8e694e2388aed90f157fcf8f53a4793970e1014
Jeel Bhensadadiya <jeel.official.24@gmail.com>
2023-11-16 18:39:39 +0530

    Create program that demonstrate how to create class and object or how to use object

---
+++ src/Java_Class_Object.java
@@ -0,0 +1,19 @@
+
+// Create Class for Car Details or Info
+class carsDetails{
+    String cName = "Mercedes-Benz";
+    int cQuantity = 23;
+    int cModel = 2023;
+}
+
+public class Java_Class_Object {
+    public static void main(String[] args) {
+        // Make carsDetails class Object
+        // Create object obj instance of carDetails class
+        carsDetails obj = new carsDetails();
+
+        System.out.println("Car Name : " + obj.cName);
+        System.out.println("Car MFG Year : " + obj.cModel);
+        System.out.println("Car Total Quantity : " + obj.cQuantity);
+    }
+}
